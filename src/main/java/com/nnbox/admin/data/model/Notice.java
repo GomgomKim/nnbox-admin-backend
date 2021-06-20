2685b4a121752453ae93a0c3ff079b0082f6fb34
kiyeun <dhfwlsjfz@blict.co.kr>
2021-06-20 14:45:31 +0900

    공지사항 변수별 설명 추가

--- src/main/java/com/nnbox/admin/data/model/Notice.java
+++ src/main/java/com/nnbox/admin/data/model/Notice.java
@@ -8,21 +8,23 @@ public class Notice {
     private Integer idx;
 
     // 제목
-    @ApiModelProperty(value = "제목", dataType = "String")
+    @ApiModelProperty(value = "제목", dataType = "String", example = "title")
     private String title;
 
     // 내용
-    @ApiModelProperty(value = "내용", dataType = "String")
+    @ApiModelProperty(value = "내용", dataType = "String", example = "content")
     private String content;
 
     // 공지 생성 날짜
-    @ApiModelProperty(value = "공지 생성 날짜", dataType = "String")
+    @ApiModelProperty(value = "공지 생성 날짜", dataType = "String", example = "2021-06-01")
     private String createDate;
 
     // 삭제 여부
+    @ApiModelProperty(value = "삭제 여부", dataType = "Integer", example = "0")
 	private Byte deleted;
 	
 	// 삭제 일자
+    @ApiModelProperty(value = "삭제 일자", dataType = "String", example = "2021-06-01")
     private String deleteDate;
 
 	// 카테고리(1: 전체, 2: 라이더, 3: 가맹점)
