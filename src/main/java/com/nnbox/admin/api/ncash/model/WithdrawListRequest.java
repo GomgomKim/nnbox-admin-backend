b7fdcf6afdac4cc72108cf6528f9d339a3ddeafc
kiyeun <dhfwlsjfz@blict.co.kr>
2021-06-22 15:17:18 +0900

    예치금 출금내역 유저 유형 선택 추가

--- src/main/java/com/nnbox/admin/api/ncash/model/WithdrawListRequest.java
+++ src/main/java/com/nnbox/admin/api/ncash/model/WithdrawListRequest.java
@@ -12,4 +12,8 @@ public class WithdrawListRequest extends ListRequest {
 	// 유저 ID
     @ApiModelProperty(value = "유저 ID", dataType = "String", example = "userId")
     private String userId;
+    
+    // 유저 유형
+    @ApiModelProperty(value = "유저 유형(1: 라이더, 2: 가맹점)", dataType = "Integer", example = "1", required = true, hidden = false)
+    private Integer userType;
 }
