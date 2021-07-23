6816882cdd4da12416de8058e64caadcc4414c7d
kiyeun <dhfwlsjfz@blict.co.kr>
2021-07-23 11:54:56 +0900

    예치금 지급 파라미터에 유저 아이디 추가

--- src/main/java/com/nnbox/admin/api/ncash/model/NcashCreateRequest.java
+++ src/main/java/com/nnbox/admin/api/ncash/model/NcashCreateRequest.java
@@ -11,6 +11,9 @@ import lombok.Setter;
 public class NcashCreateRequest extends Log {
   @ApiModelProperty(value = "받는 유저(라이더 or 가맹점) IDX", dataType = "Integer", example = "2")
   private Integer receiveUserIdx;
+  
+  @ApiModelProperty(value = "아이디", dataType = "String", example = "knowend")
+  private String userId;
 
   @ApiModelProperty(value = "보낼 금액(ncash)", dataType = "Integer", example = "1")
   private Integer ncashAmount;
